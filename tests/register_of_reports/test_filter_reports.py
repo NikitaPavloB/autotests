92e4cdbb0509c48b3280bd0b8c0422648c4d54e7
Nikita <kindpavlov@yandex.ru>
2025-03-04 17:54:55 +0300

    Добавлена сортировка во все уровни видимости

--- tests/register_of_reports/test_filter_reports.py
+++ tests/register_of_reports/test_filter_reports.py
@@ -45,7 +45,8 @@ def test_filter_organization_level(browser, login):
         test_page.click_approved_btn()
         test_page.click_periodicity_btn()
         test_page.click_monthly_btn()
-        test_page.click_period_btn()
+        time.sleep(2)
+        test_page.click_period_data_btn()
         test_page.click_year_calendar_btn()
         test_page.enter_starting_year_calendar('2054')
         test_page.enter_end_year_calendar('2054')
@@ -83,6 +84,21 @@ def test_filter_district_city_level(browser):
         if not check.equal(test_page.get_check_district_city_veshnyaki(), " муниципальный округ Вешняки ",
                            "Проверка успешной фильтрации района-города"):
             save_screenshot_on_check_fail(browser,'Проверка фильтрации по району-города')
+    with allure.step("Проверка фильтрации по состоянию, периодичности и периоду"):
+        test_page.click_status_btn()
+        test_page.click_approved_btn()
+        test_page.click_periodicity_btn()
+        test_page.click_monthly_btn()
+        time.sleep(2)
+        test_page.click_period_year_btn()
+        test_page.click_year_calendar_btn()
+        test_page.enter_starting_year_calendar('2054')
+        test_page.enter_end_year_calendar('2054')
+    with allure.step("Проверка успешной фильтрации по состоянию и периоду"):
+        if not check.equal(test_page.get_check_sort_status(), "Утвержден", "Выбран статус 'Утвержден'"):
+            save_screenshot_on_check_fail(browser, "Проверка состояния")
+        if not check.is_true("2054" in test_page.get_check_sort_period()):
+            save_screenshot_on_check_fail(browser, "Проверка сортировки года")
     logging.info('Закончили тест проверки фильтрации на уровне район-города')
     with allure.step("Сбросить все фильтры в реестре"):
         test_page.click_reset_filters_btn()
@@ -103,12 +119,27 @@ def test_filter_district_level(browser):
     with allure.step("Выбор региона и района"):
         test_page.click_region_field()
         test_page.click_msk_btn()
-        time.sleep(2)
+        time.sleep(3)
         test_page.click_district_field()
         test_page.click_vostok_btn()
     with allure.step("Проверка успешной фильтрации на районном уровне"):
         if not check.equal(test_page.get_check_filter_district(), "Восточный", "Проверка успешной фильтрации района"):
             save_screenshot_on_check_fail(browser,'Проверка фильтрации по району')
+    with allure.step("Проверка фильтрации по состоянию, периодичности и периоду"):
+        test_page.click_status_btn()
+        test_page.click_approved_btn()
+        test_page.click_periodicity_btn()
+        test_page.click_monthly_btn()
+        time.sleep(2)
+        test_page.click_period_year_btn()
+        test_page.click_year_calendar_btn()
+        test_page.enter_starting_year_calendar('2054')
+        test_page.enter_end_year_calendar('2054')
+    with allure.step("Проверка успешной фильтрации по состоянию и периоду"):
+        if not check.equal(test_page.get_check_sort_status(), "Утвержден", "Выбран статус 'Утвержден'"):
+            save_screenshot_on_check_fail(browser, "Проверка состояния")
+        if not check.is_true("2054" in test_page.get_check_sort_period()):
+            save_screenshot_on_check_fail(browser, "Проверка сортировки года")
     logging.info('Закончили тест проверки фильтрации районного уровня')
     with allure.step("Сбросить все фильтры в реестре"):
         test_page.click_reset_filters_btn()
@@ -127,12 +158,28 @@ def test_filter_region_level(browser):
         if not check.equal(test_page.get_check_region_lvl(),'Регион', 'Проверка, что находимся на региональном уровне'):
             save_screenshot_on_check_fail(browser, 'Проверка регионального уровня')
     with allure.step("Выбор региона"):
+        time.sleep(1)
         test_page.click_region_field()
         test_page.click_msk_btn()
         time.sleep(2)
     with allure.step("Проверка успешной фильтрации на региональном уровне"):
         if not check.equal(test_page.get_check_filter_region(), "г. Москва", "Проверка успешной фильтрации региона"):
             save_screenshot_on_check_fail(browser,'Проверка фильтрации по региону')
+    with allure.step("Проверка фильтрации по состоянию, периодичности и периоду"):
+        test_page.click_status_btn()
+        test_page.click_approved_btn()
+        test_page.click_periodicity_btn()
+        test_page.click_monthly_btn()
+        time.sleep(2)
+        test_page.click_period_year_btn()
+        test_page.click_year_calendar_btn()
+        test_page.enter_starting_year_calendar('2054')
+        test_page.enter_end_year_calendar('2054')
+    with allure.step("Проверка успешной фильтрации по состоянию и периоду"):
+        if not check.equal(test_page.get_check_sort_status(), "Утвержден", "Выбран статус 'Утвержден'"):
+            save_screenshot_on_check_fail(browser, "Проверка состояния")
+        if not check.is_true("2054" in test_page.get_check_sort_period()):
+            save_screenshot_on_check_fail(browser, "Проверка сортировки года")
     logging.info('Закончили тест проверки фильтрации на региональном уровне')
 
 
