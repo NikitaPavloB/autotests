f04a45b3a4a07873ecbeb3a84de4870e4a8483cd
Nikita <kindpavlov@yandex.ru>
2025-03-04 17:51:40 +0300

    Отредактировал описание методов/локаторÐов

--- pages/login/login_page.py
+++ pages/login/login_page.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 import allure
 
 
+# Локаторы для страницы авторизации
+
+
 class LoginLocators:
-    """Локаторы для страницы авторизации"""
     LOCATOR_LOGIN_FIELD = (By.ID, "login")
     LOCATOR_PASSWORD_FIELD = (By.ID, "password")
     LOCATOR_LOGIN_BTN = (By.ID, "submit-button")
@@ -17,19 +19,19 @@ class LoginPage(BasePage):
     # Ввод текста
     @allure.step('Ввод логина: {word}')
     def enter_login(self, word):
-        self.enter_text_into_field(LoginLocators.LOCATOR_LOGIN_FIELD, word, description="login form")
+        self.enter_text_into_field(LoginLocators.LOCATOR_LOGIN_FIELD, word, description="Поле 'Логин'")
 
     @allure.step('Ввод пароля: {word}')
     def enter_password(self, word):
-        self.enter_text_into_field(LoginLocators.LOCATOR_PASSWORD_FIELD, word, description="password form")
+        self.enter_text_into_field(LoginLocators.LOCATOR_PASSWORD_FIELD, word, description="Поле 'Пароль'")
 
     # Клик на кнопку
     @allure.step("Клик на кнопку 'Войти'")
     def click_login_button(self):
-        self.click_button(LoginLocators.LOCATOR_LOGIN_BTN, description="login")
+        self.click_button(LoginLocators.LOCATOR_LOGIN_BTN, description="Кнопка 'Войти'")
 
     # Получение текста
     @allure.step("Проверяем успешную авторизацию пользователя")
     def get_menu_text(self):
-        return self.get_text_from_element(LoginLocators.LOCATOR_MENU_FIELD, description="Check menu field")
+        return self.get_text_from_element(LoginLocators.LOCATOR_MENU_FIELD, description="Текст меню после авторизации")
 
