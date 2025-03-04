2be0ca1d2dce26b698221a14ef57c0201fefa34d
Nikita <kindpavlov@yandex.ru>
2025-03-04 17:53:34 +0300

    Отредактировал описание методов/локаторÐÐÐÐов

--- pages/register_of_reports/filter_reports_page.py
+++ pages/register_of_reports/filter_reports_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import allure
 
 
-class TestSearchLocators:
+class ReportRegistersLocators:
     LOCATOR_LOGIN_FIELD = (By.ID, "login")
     LOCATOR_PASSWORD_FIELD = (By.ID, "password")
     LOCATOR_LOGIN_BTN = (By.ID, "submit-button")
@@ -105,193 +105,194 @@ class OperationsHelper(BasePage):
     # Ввод текста
     @allure.step('Ввод логина: {word}')
     def enter_login(self, word):
-        self.enter_text_into_field(TestSearchLocators.LOCATOR_LOGIN_FIELD, word, description="Логин")
+        self.enter_text_into_field(ReportRegistersLocators.LOCATOR_LOGIN_FIELD, word, description="Поле 'Логин'")
 
     @allure.step('Ввод пароля: {word}')
     def enter_password(self, word):
-        self.enter_text_into_field(TestSearchLocators.LOCATOR_PASSWORD_FIELD, word, description="Пароль")
+        self.enter_text_into_field(ReportRegistersLocators.LOCATOR_PASSWORD_FIELD, word, description="Поле 'Пароль'")
 
     @allure.step('Ввод названия организации: {word}')
     def enter_org_name(self, word):
-        self.enter_text_into_field(TestSearchLocators.LOCATOR_ORG_NAME, word,
+        self.enter_text_into_field(ReportRegistersLocators.LOCATOR_ORG_NAME, word,
                                    description="Название организации")
 
-    @allure.step('Ввод года (начальный) в календарь: {word}')
+    @allure.step("Ввод начального года: {word}")
     def enter_starting_year_calendar(self, word):
-        self.enter_text_into_field(TestSearchLocators.LOCATOR_START_YEAR_FIELD, word, description="Начальный год", press_enter=True)
+        self.enter_text_into_field(ReportRegistersLocators.LOCATOR_START_YEAR_FIELD, word, description="Поле 'Начальный год'", press_enter=True)
 
-    @allure.step('Ввод года (конечный) в календарь: {word}')
+    @allure.step("Ввод конечного года: {word}")
     def enter_end_year_calendar(self, word):
-        self.enter_text_into_field(TestSearchLocators.LOCATOR_END_YEAR_FIELD, word, description="Конечный год", press_enter=True)
+        self.enter_text_into_field(ReportRegistersLocators.LOCATOR_END_YEAR_FIELD, word, description="Поле 'Конечный год'", press_enter=True)
 
     # Клик на кнопку
     @allure.step("Клик на кнопку 'Войти'")
     def click_login_button(self):
-        self.click_button(TestSearchLocators.LOCATOR_LOGIN_BTN, description="login")
+        self.click_button(ReportRegistersLocators.LOCATOR_LOGIN_BTN, description="Кнопка 'Войти'")
 
     @allure.step("Клик на иконку 'СТАТ отчетность'")
     def click_icon_stat(self):
-        self.click_button(TestSearchLocators.LOCATOR_ICON_STAT, description="Icon STAT")
+        self.click_button(ReportRegistersLocators.LOCATOR_ICON_STAT, description="Иконка 'СТАТ отчетность'")
 
     @allure.step("Клик на раздел 'Мониторинг здоровья'")
     def click_health_monitoring(self):
-        self.click_button(TestSearchLocators.locator_health_monitoring, description="Раздел 'Мониторинг здоровья'")
+        self.click_button(ReportRegistersLocators.locator_health_monitoring, description="Раздел 'Мониторинг здоровья'")
 
     @allure.step("Клик на 'Форма №1'")
     def click_form_1(self):
-        self.click_button(TestSearchLocators.LOCATOR_FORM_1, description="Form №1")
+        self.click_button(ReportRegistersLocators.LOCATOR_FORM_1, description="Кнопка 'Форма №1'")
 
     @allure.step("Клик на выпадающий список уровней видимости")
     def click_level(self):
-        self.click_button(TestSearchLocators.LOCATOR_LEVEL, description="Выпадающий список уровней видимости")
+        self.click_button(ReportRegistersLocators.LOCATOR_LEVEL, description="Выпадающий список уровней видимости")
 
-    @allure.step("Клик на организационный уровень видимости")
+    @allure.step("Выбор уровня видимости: 'Организация'")
     def click_level_organization(self):
-        self.click_button(TestSearchLocators.LOCATOR_LEVEL_ORGANIZATION,
-                          description='Уровень видимости "Организация"')
+        self.click_button(ReportRegistersLocators.LOCATOR_LEVEL_ORGANIZATION,
+                          description="Выбор уровня 'Организация'")
 
-    @allure.step("Клик на уровень видимости 'Район-города")
+    @allure.step("Выбор уровня видимости: 'Район-города'")
     def click_level_district_city(self):
-        self.click_button(TestSearchLocators.LOCATOR_LEVEL_DISTRICT_CITY,
-                          description='Уровень видимости "Район-города"')
+        self.click_button(ReportRegistersLocators.LOCATOR_LEVEL_DISTRICT_CITY,
+                          description="Выбор уровня 'Район-города'")
 
-    @allure.step("Клик на районный уровень видимости")
+    @allure.step("Выбор уровня видимости: 'Районный'")
     def click_level_district(self):
-        self.click_button(TestSearchLocators.LOCATOR_LEVEL_DISTRICT,
-                          description='Уровень видимости "Районный"')
+        self.click_button(ReportRegistersLocators.LOCATOR_LEVEL_DISTRICT,
+                          description="Выбор уровня 'Районный'")
 
-    @allure.step("Клик на региональный уровень видимости")
+    @allure.step("Выбор уровня видимости: 'Региональный'")
     def click_level_region(self):
-        self.click_button(TestSearchLocators.LOCATOR_LEVEL_REGIONAL,
-                          description='Уровень видимости "Региональный"')
+        self.click_button(ReportRegistersLocators.LOCATOR_LEVEL_REGIONAL,
+                          description="Выбор уровня 'Региональный'")
 
     @allure.step("Клик на поле 'Регион' в реестре отчетов")
     def click_region_field(self):
-        self.click_button(TestSearchLocators.LOCATOR_REGIONAL_FIELD, description="Регион (выпадающий список)")
+        self.click_button(ReportRegistersLocators.LOCATOR_REGIONAL_FIELD, description="Регион (выпадающий список)")
 
     @allure.step("Выбираем регион из выпадающего списка: 'Москва'")
     def click_msk_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_MSK_BTN, description="Москва из выпадающего списка")
+        self.click_button(ReportRegistersLocators.LOCATOR_MSK_BTN, description="Москва из выпадающего списка")
 
     @allure.step("Клик на поле 'Район' в реестре отчетов")
     def click_district_field(self):
-        self.click_button(TestSearchLocators.LOCATOR_DISTRICT_FIELD, description="Район (выпадающий список)")
+        self.click_button(ReportRegistersLocators.LOCATOR_DISTRICT_FIELD, description="Район (выпадающий список)")
 
     @allure.step("Выбираем район из выпадающего списка: 'Восточный'")
     def click_vostok_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_VOSTOK_BTN, description="Восточный район Москва")
+        self.click_button(ReportRegistersLocators.LOCATOR_VOSTOK_BTN, description="Восточный район Москва")
 
     @allure.step("Клик на поле 'Организация' в реестре отчетов")
     def click_organization_field(self):
-        self.click_button(TestSearchLocators.LOCATOR_ORGANIZATION_FIELD, description="Выпадающий список организаций")
+        self.click_button(ReportRegistersLocators.LOCATOR_ORGANIZATION_FIELD, description="Выпадающий список организаций")
 
     @allure.step("Клик на поле 'Район-города' в реестре отчетов")
     def click_district_city_field(self):
-        self.click_button(TestSearchLocators.LOCATOR_DISTRICT_CITY_FIELD, description="Выпадающий список район-города")
+        self.click_button(ReportRegistersLocators.LOCATOR_DISTRICT_CITY_FIELD, description="Выпадающий список район-города")
 
     @allure.step("Выбираем организацию из выпадающего списка: 'Альбион'")
     def click_albion_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_ORG_ALBION, description="Организация 'Альбион")
+        self.click_button(ReportRegistersLocators.LOCATOR_ORG_ALBION, description="Организация 'Альбион")
 
     @allure.step("Выбираем район-города из выпадающего списка: 'муниципальный округ Вешняки'")
     def click_veshnyaki_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_DISTRICT_CITY_VESHNYAKI, description="Район-города 'муниципальный округ Вешняки'")
+        self.click_button(ReportRegistersLocators.LOCATOR_DISTRICT_CITY_VESHNYAKI, description="Район-города 'муниципальный округ Вешняки'")
 
     @allure.step("Клик на поле 'Состояние' в реестре отчетов")
     def click_status_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_STATUS_FIELD, description='Поле "Состояние" в реестре отчетов')
+        self.click_button(ReportRegistersLocators.LOCATOR_STATUS_FIELD, description='Поле "Состояние" в реестре отчетов')
 
     @allure.step("Выбираем состояние из выпадающего списка: 'Утверждён'")
     def click_approved_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_APPROVED_BTN, description="Статус 'Утверждён'")
+        self.click_button(ReportRegistersLocators.LOCATOR_APPROVED_BTN, description="Статус 'Утверждён'")
 
     @allure.step("Клик на поле 'Периодичность' в реестре отчетов")
     def click_periodicity_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_PERIODICITY_FIELD, description='Поле "Периодичность" в реестре отчетов')
+        self.click_button(ReportRegistersLocators.LOCATOR_PERIODICITY_FIELD, description='Поле "Периодичность" в реестре отчетов')
 
     @allure.step("Выбираем периодичность из выпадающего списка: 'Ежемесячная'")
     def click_monthly_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_MONTHLY_BTN, description="Периодичность 'Ежемесячная'")
+        self.click_button(ReportRegistersLocators.LOCATOR_MONTHLY_BTN, description="Периодичность 'Ежемесячная'")
 
-    @allure.step("Клик на поле 'Период' в реестре отчетов")
-    def click_period_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_START_DATE_FIELD, description='Поле "Период" в реестре отчетов')
+    @allure.step("Клик на поле 'Период' в реестре отчетов - дата")
+    def click_period_data_btn(self):
+        self.click_button(ReportRegistersLocators.LOCATOR_START_DATE_FIELD, description='Поле "Период" в реестре отчетов')
+
+    @allure.step("Клик на поле 'Период' в реестре отчетов - год")
+    def click_period_year_btn(self):
+        self.click_button(ReportRegistersLocators.LOCATOR_START_YEAR_FIELD, description='Поле "Период" в реестре отчетов-год')
 
     @allure.step("Выбираем фильтрацию в календаре: 'Год'")
     def click_year_calendar_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_YEAR_BTN, description='Календарь фильтр "Год"')
+        self.click_button(ReportRegistersLocators.LOCATOR_YEAR_BTN, description='Календарь фильтр "Год"')
 
     @allure.step("Клик на кнопку 'Сбросить фильтры'")
     def click_reset_filters_btn(self):
-        self.click_button(TestSearchLocators.LOCATOR_RESET_FILTERS_BTN, description="Кнопка сброса фильтров")
+        self.click_button(ReportRegistersLocators.LOCATOR_RESET_FILTERS_BTN, description="Кнопка 'Сбросить фильтры'")
 
     # Получение текста
     @allure.step("Проверяем успешную авторизацию пользователя")
     def get_menu_text(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_MENU_FIELD, description="Check menu field")
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_MENU_FIELD, description="Текст меню после авторизации")
 
-    @allure.step("Проверяем успешный переход на организационный уровень")
+    @allure.step("Проверяем успешный переход на уровень 'Организация'")
     def get_check_organization_lvl(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_CHECK_ORGANIZATION,
-                                          description="Проверка организации")
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_CHECK_ORGANIZATION,
+                                          description="Подтверждение уровня 'Организация'")
 
-    @allure.step("Проверяем успешный переход на районный уровень")
-    def get_check_district_lvl(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_CHECK_DISTRICT,
-                                          description="Проверка перехода на районный уровень")
-
-    @allure.step("Проверяем успешный переход уровень 'район-города'")
+    @allure.step("Проверяем успешный переход на уровень 'Район-города'")
     def get_check_district_city_lvl(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_CHECK_DISTRICT_CITY,
-                                          description="Проверка перехода на уровень 'район-города'")
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_CHECK_DISTRICT_CITY,
+                                          description="Подтверждение уровня 'Район-города'")
 
-    @allure.step("Проверяем успешный переход на региональный уровень")
+    @allure.step("Проверяем успешный переход на уровень 'Районный'")
+    def get_check_district_lvl(self):
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_CHECK_DISTRICT,
+                                          description="Подтверждение уровня 'Районный'")
+
+    @allure.step("Проверяем успешный переход на уровень 'Региональный'")
     def get_check_region_lvl(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_CHECK_REGION,
-                                          description="Проверка перехода на региональный уровень")
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_CHECK_REGION,
+                                          description="Подтверждение уровня 'Региональный'")
 
     @allure.step("Проверяем успешную фильтрацию по организации: 'Альбион'")
     def get_check_organization_albion(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_ORG_ALBION_FIELD,
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_ORG_ALBION_FIELD,
                                           description="Успешная проверка фильтрации организаций")
 
     @allure.step("Проверяем успешную фильтрацию по району-города")
     def get_check_district_city_veshnyaki(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_DISTRICT_CITY_VESHNYAKI,
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_DISTRICT_CITY_VESHNYAKI,
                                           description="Успешная проверка фильтрации района-города")
 
     @allure.step("Проверяем успешную фильтрацию по району")
     def get_check_filter_district(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_CHECK_DISTRICT_FIELD,
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_CHECK_DISTRICT_FIELD,
                                           description="Успешная проверка фильтрации по району")
 
     @allure.step("Проверяем успешную фильтрацию по региону")
     def get_check_filter_region(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_CHECK_REGION_FIELD,
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_CHECK_REGION_FIELD,
                                           description="Успешная проверка фильтрации по региону")
 
     @allure.step("Проверка, что выбраны все регионы")
     def get_check_all_reg(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_REGIONAL_FIELD, description="Выбраны все регионы")
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_REGIONAL_FIELD, description="Выбраны все регионы")
 
     @allure.step("Проверяем успешную фильтрацию по состоянию")
     def get_check_sort_status(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_SORT_CHECK_FIELD,
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_SORT_CHECK_FIELD,
                                           description="Успешная проверка фильтрации по состоянию")
 
     @allure.step("Проверяем успешную фильтрацию по периоду")
     def get_check_sort_period(self):
-        return self.get_text_from_element(TestSearchLocators.LOCATOR_PERIOD_CHECK_FIELD,
+        return self.get_text_from_element(ReportRegistersLocators.LOCATOR_PERIOD_CHECK_FIELD,
                                           description="Успешная проверка фильтрации по периоду")
-
-    def get_check_vostok_btn(self):
-        self.get_text_from_element(TestSearchLocators.LOCATOR_VOSTOK_BTN, description="Восточный район")
 
     # Выпадающие списки
 
     def select_dropdown_regional(self, text):
-        return self.select_from_dropdown(TestSearchLocators.LOCATOR_DROPDOWN_REGIONAL, text,
-                                                                description="Region dropdown list")
+        return self.select_from_dropdown(ReportRegistersLocators.LOCATOR_DROPDOWN_REGIONAL, text,
+                                         description="Region dropdown list")
 
 
 
