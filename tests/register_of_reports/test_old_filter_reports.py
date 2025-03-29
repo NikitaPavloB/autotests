ab83428b25391994338bc38f220a58f3518ff26b
Nikita <kindpavlov@yandex.ru>
2025-03-29 13:32:14 +0300

    Extend tests: federal level + sequential marker

--- tests/register_of_reports/test_old_filter_reports.py
+++ tests/register_of_reports/test_old_filter_reports.py
@@ -1,20 +1,19 @@
 import time
-from pages.register_of_reports.filter_reports_page import OperationsHelper
 import logging
 import allure
+import pytest
 import pytest_check as check
 from tests.conftest import save_screenshot_on_check_fail
+from pages.register_of_reports.filter_old_reports_page import RegisterOldOperationsHelper
+
+pytest_mark = pytest.mark.order(1)
 
 
-@allure.feature('Фильтрация в реестре отчетов')
+@allure.feature('Фильтрация в реестре отчетов - старая логика')
 @allure.story('1. Фильтрация на организационном уровне')
 def test_filter_organization_level(browser, login):
     logging.info('Запустили тест проверки фильтрации на организационном уровне')
-    test_page = OperationsHelper(browser)
-    with allure.step('Проверка успешной авторизации'):
-        # assert test_page.get_menu_text() == 'Доступные для работы модули:'
-        if not check.equal(test_page.get_menu_text(),'Доступные для работы модули:', 'Проверка текста меню'):
-            save_screenshot_on_check_fail(browser, 'Проверка авторизации')
+    test_page = RegisterOldOperationsHelper(browser)
     with allure.step("Переход к модулю 'Мониторинг здоровья'"):
         test_page.click_icon_stat()
         test_page.click_health_monitoring()
@@ -23,7 +22,6 @@ def test_filter_organization_level(browser, login):
     with allure.step("Выбор уровня видимости 'Организация'"):
         test_page.click_level()
         test_page.click_level_organization()
-        # assert test_page.get_check_organization_lvl() == 'Организация'
         if not check.equal(test_page.get_check_organization_lvl(),'Организация', 'Проверка, что находимся на орг.уровне'):
             save_screenshot_on_check_fail(browser, 'Проверка уровня организации')
     time.sleep(5)
@@ -37,7 +35,6 @@ def test_filter_organization_level(browser, login):
         test_page.enter_org_name('альбион')
         test_page.click_albion_btn()
     with allure.step("Проверка выбранной организации"):
-        # assert test_page.get_check_organization_albion() == 'ООО "АЛЬБИОН"'
         if not check.equal(test_page.get_check_organization_albion(), 'ООО "АЛЬБИОН"', 'Проверка успешной фильтрации орг.'):
             save_screenshot_on_check_fail(browser, 'Проверка Альбиона')
     with allure.step("Проверка сортировки по состоянию, периодичности и периоду"):
@@ -62,11 +59,11 @@ def test_filter_organization_level(browser, login):
             save_screenshot_on_check_fail(browser,"Проверка кнопки сброса фильтров")
 
 
-@allure.feature('Фильтрация в реестре отчетов')
+@allure.feature('Фильтрация в реестре отчетов - старая логика')
 @allure.story('2. Фильтрация на уровне район-города')
 def test_filter_district_city_level(browser):
     logging.info('Запустили тест проверки фильтрации на уровне район-города')
-    test_page = OperationsHelper(browser)
+    test_page = RegisterOldOperationsHelper(browser)
     with allure.step("Выбор уровня видимости 'Район-города'"):
         test_page.click_level()
         test_page.click_level_district_city()
@@ -106,11 +103,11 @@ def test_filter_district_city_level(browser):
             save_screenshot_on_check_fail(browser,"Проверка кнопки сброса фильтров")
 
 
-@allure.feature('Фильтрация в реестре отчетов')
+@allure.feature('Фильтрация в реестре отчетов - старая логика')
 @allure.story('3. Фильтрация на районном уровне')
 def test_filter_district_level(browser):
     logging.info('Запустили тест проверки фильтрации на районном уровне')
-    test_page = OperationsHelper(browser)
+    test_page = RegisterOldOperationsHelper(browser)
     with allure.step("Выбор уровня видимости 'Районный'"):
         test_page.click_level()
         test_page.click_level_district()
@@ -147,11 +144,11 @@ def test_filter_district_level(browser):
             save_screenshot_on_check_fail(browser,"Проверка кнопки сброса фильтров")
 
 
-@allure.feature('Фильтрация в реестре отчетов')
+@allure.feature('Фильтрация в реестре отчетов - старая логика')
 @allure.story('4. Фильтрация на региональном уровне')
 def test_filter_region_level(browser):
     logging.info('Запустили тест проверки фильтрации на региональном уровне')
-    test_page = OperationsHelper(browser)
+    test_page = RegisterOldOperationsHelper(browser)
     with allure.step("Выбор уровня видимости 'Региональный'"):
         test_page.click_level()
         test_page.click_level_region()
@@ -181,4 +178,37 @@ def test_filter_region_level(browser):
         if not check.is_true("2054" in test_page.get_check_sort_period()):
             save_screenshot_on_check_fail(browser, "Проверка сортировки года")
     logging.info('Закончили тест проверки фильтрации на региональном уровне')
+    with allure.step("Сбросить все фильтры в реестре"):
+        test_page.click_reset_filters_btn()
+        if not check.equal(test_page.get_check_all_reg(), "РФ", "Проверка сброса всех фильтров"):
+            save_screenshot_on_check_fail(browser,"Проверка кнопки сброса фильтров")
 
+
+@allure.feature('Фильтрация в реестре отчетов - старая логика')
+@allure.story('5. Фильтрация на федеральном уровне')
+def test_filter_federal_level(browser):
+    logging.info('Запустили тест проверки фильтрации на федеральном уровне')
+    test_page = RegisterOldOperationsHelper(browser)
+    with allure.step("Выбор уровня видимости 'Федеральный'"):
+        test_page.click_level()
+        test_page.click_level_federal()
+        if not check.equal(test_page.get_check_federal_lvl(),'РФ', 'Проверка, что находимся на федеральном уровне'):
+            save_screenshot_on_check_fail(browser, 'Проверка федерального уровня')
+    with allure.step("Проверка фильтрации по состоянию, периодичности и периоду"):
+        time.sleep(1)
+        test_page.click_status_btn()
+        test_page.click_approved_btn()
+        test_page.click_periodicity_btn()
+        test_page.click_monthly_btn()
+        time.sleep(2)
+        test_page.click_period_year_btn()
+        test_page.click_year_calendar_btn()
+        test_page.enter_starting_year_calendar('2054')
+        test_page.enter_end_year_calendar('2054')
+        time.sleep(1)
+    with allure.step("Проверка успешной фильтрации по состоянию и периоду"):
+        if not check.equal(test_page.get_check_sort_status(), "Утвержден", "Выбран статус 'Утвержден'"):
+            save_screenshot_on_check_fail(browser, "Проверка состояния")
+        if not check.is_true("2054" in test_page.get_check_sort_period()):
+            save_screenshot_on_check_fail(browser, "Проверка сортировки года")
+    logging.info('Закончили тест проверки фильтрации на региональном уровне')
